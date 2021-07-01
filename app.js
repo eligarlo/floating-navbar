7ec8079f37f9ae296354222dadf645110c1f62da
eligarlo <eligarlo.developer@gmail.com>
2021-07-01 21:06:31 +0300

    icons disappearing from left to right

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const navbar = document.querySelector('.mobile-navbar')
 const menuBtn = document.querySelector('.menu-btn')
+const firstLink = document.querySelector('.mobile-navbar .wrapper a:nth-child(1)')
+const thirdLink = document.querySelector('.mobile-navbar .wrapper a:nth-child(3)')
 
 menuBtn.addEventListener('click', () => {
+  console.log(navbar.className.includes('active'))
+  if (!navbar.className.includes('active')) {
+    firstLink.style.transitionDelay = '0.5s'
+    thirdLink.style.transitionDelay = ''
+  } else {
+    firstLink.style.transitionDelay = ''
+    thirdLink.style.transitionDelay = '0.5s'
+  }
   navbar.classList.toggle('active')
 })
